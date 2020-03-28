da5ad78bd00965dc5468f5868e44ae0460e476fb
nhutan0000 <huynhnhutan240700@gamil.com>
2020-03-28 20:39:08 +0700

    first

---
+++ Node.java
@@ -0,0 +1,11 @@
+class Node{
+     Integer key;
+     Node left,right;
+     int size;
+     int height;
+    public Node( Integer key, int size,int height){
+        this.key=key;
+        this.size=size;
+        this.height=height;
+    }
+}
